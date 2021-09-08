3ce6b6c05cc6bc11823f64f79d892b27fb517c95
chae-young <cycy8527@gmail.com>
2021-09-08 23:53:06 +0900

    Add theme

---
+++ styles/theme.js
@@ -0,0 +1,28 @@
+const deviceSizes = {
+  mobileS: "320px",
+  tablet: "767px",
+  tabletL: "1024px",
+  desktop: "1200px",
+}
+
+const device = {
+  mobileS: `@media screen and (max-width: ${deviceSizes.mobileS})`,
+  mobileL: `@media screen and (max-width: ${deviceSizes.tablet})`,
+  tabletS: `@media screen and (max-width: ${deviceSizes.desktop})`,
+  tabletL: `@media screen and (min-width: ${deviceSizes.tablet}) and (max-width: ${deviceSizes.desktop})`,
+  desktop: `@media screen and (min-width: ${deviceSizes.desktop})`,
+}
+const colors = {
+  black: "rgb(0,0,0)",
+  white: "rgb(255,255,255)",
+  gray: "rgb(103,103,103)",
+  gray_2: "#767676",
+  green_1: "#3cb46e",
+}
+const theme = {
+  colors,
+  deviceSizes,
+  device,
+}
+
+export default theme
